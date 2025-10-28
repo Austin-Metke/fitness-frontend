f30686ce44fe42993cad8cfd59a0056e7149084b
gabe-tommi <gabetgal2112@gmail.com>
2025-10-28 09:27:04 -0700

    added exercise search functionality. Currently returns all results instead of getting a search by name, but it does work

--- api/workOutAPI.ts
+++ api/workOutAPI.ts
@@ -2,25 +2,40 @@
 import axios from 'axios';
 
 //function for grabbing exercises
-export const getExercises = async (params = {}) => {
-    const options = {
-        method: 'GET',
-        url: 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises',
-        params: { ...params },
-        headers: {
-            'x-rapidapi-key': '54ec59fd7amsh724c1940bbcd393p111f66jsnd567f0225d39',
-            'x-rapidapi-host': 'exercises-by-api-ninjas.p.rapidapi.com'
-        }
-    };
+// export const getExercises = async (params = {}) => {
+//     const options = {
+//         method: 'GET',
+//         // url: 'https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises',
+//         params: { ...params },
+//         // headers: {
+//         //     'x-rapidapi-key': '54ec59fd7amsh724c1940bbcd393p111f66jsnd567f0225d39',
+//         //     'x-rapidapi-host': 'exercises-by-api-ninjas.p.rapidapi.com'
+//         // }
+//         url: 'https://localhost:8080/api/exercises/muscles/{$params}'
+//     };
 
-    try {
-        const response = await axios.request(options);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
+//     try {
+//         const response = await axios.request(options);
+//         return response.data;
+//     } catch (error) {
+//         console.error(error);
+//         throw error;
+//     }
+// };
+
+export const getExercises = async (exerciseSearch: string) => {
+  try {
+    const response = await axios.get(`http://10.0.2.2:8080/api/exercises/exercise`, {
+      params: { name: exerciseSearch },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching exercises:", error);
+    throw error;
+  }
 };
+
+
 
 
 //function for grabbing exercise details
